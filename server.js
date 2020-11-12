94040d78295fe87d5f73cbc358f32d6f10e3b710
vishal <vj9822god@gmail.com>
2020-11-12 13:48:06 +0530

    nodejs

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 
 const io = require('socket.io')(8000)
+const port = process.env.PORT || 8000;
 
+const http = require('http');
+
+const hostname = '127.0.0.1';
+
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
 // require('browserify') (8000)
 const users = {};
 io.on('connection', socket =>{
